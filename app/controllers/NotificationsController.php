d9d8a13b56ed0ed0dc1f838796f1473da91093d7
jsyzchen <jsyzchenchen@gmail.com>
2017-06-25 18:24:10 +0800

    update

---
+++ app/controllers/NotificationsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+/**
+ * 通知控制器
+ * @author jsyzchenchen@gmail.com
+ * @date 2017/06/25
+ */
+class NotificationsController extends ControllerBase
+{
+    public function countAction()
+    {
+        exit('0');
+    }
+}
+
